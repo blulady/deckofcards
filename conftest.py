8210b6c8a38a2f0810bd8c5c27a0a121bc31d1b7
Sarah Sanger <blulady@gmail.com>
2022-05-19 19:29:05 -0700

    Delete conftest.py

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="file")
-def new_deck():
-    """returns a new deck from https://deckofcardsapi.com/"""
-    deck_response = requests.get(f"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2")
-    return deck_response.json()
