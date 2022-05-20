8e063212a66cfab5cb8624d939a8fb0cd1b4d339
ssanger <blulady@gmail.com>
2022-05-19 19:11:45 -0700

    just checking

--- test_carddeck.py
+++ test_carddeck.py
@@ -4,12 +4,12 @@ from classes_sandbox import CardDeck
 
 def test_card_deck_response():
     deck = CardDeck(1)
-    assert deck.deck["success"] == True
+    assert deck.deck["success"] is True
 
 
 def test_deck_id():
     deck = CardDeck(1)
-    assert type(deck.deck_id) == str
+    assert isinstance(deck.deck_id, str) is True
 
 
 def test_one_deck_remaining():
@@ -19,6 +19,6 @@ def test_one_deck_remaining():
 
 def test_two_decks_remaining():
     two_decks = CardDeck(2)
-    assert two_decks.deck_remaining == 104
+    assert two_decks.deck_remaining is 104
 
 
